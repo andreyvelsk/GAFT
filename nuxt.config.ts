3ade6e8099c67bc1fdb1828fc4510e0ed658e2cd
andrey_velsk <andrey_velsk@mail.ru>
2026-06-15 22:50:17 +0300

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,13 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en'
       },
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ],
       meta: [
         { name: 'yandex-verification', content: '' },
         { name: 'theme-color', content: '#11111b' }
