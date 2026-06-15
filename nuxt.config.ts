e32c06830f7c9f88c00ebb82e8645c1e0683bf34
andrey_velsk <andrey_velsk@mail.ru>
2026-06-15 23:07:56 +0300

    favicon fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,11 +40,11 @@ export default defineNuxtConfig({
         lang: 'en'
       },
       link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'manifest', href: '/site.webmanifest' }
+        { rel: 'icon', type: 'image/svg+xml', href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon.svg` },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon-32x32.png` },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon-16x16.png` },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: `${process.env.NUXT_APP_BASE_URL || '/'}apple-touch-icon.png` },
+        { rel: 'manifest', href: `${process.env.NUXT_APP_BASE_URL || '/'}site.webmanifest` }
       ],
       meta: [
         { name: 'yandex-verification', content: '' },
